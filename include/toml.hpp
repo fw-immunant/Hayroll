c2d3b8987c42ba372261f2c62fc09964e3e181b5
Frances Wingerter <fw@immunant.com>
2026-04-14 15:09:01 -0400

    include/toml: fix C++23 warning

--- include/toml.hpp
+++ include/toml.hpp
@@ -16677,12 +16677,12 @@ inline namespace literals
 inline namespace toml_literals
 {
 
-::toml::value operator"" _toml(const char* str, std::size_t len);
+::toml::value operator""_toml(const char* str, std::size_t len);
 
 #if defined(TOML11_HAS_CHAR8_T)
 // value of u8"" literal has been changed from char to char8_t and char8_t is
 // NOT compatible to char
-::toml::value operator"" _toml(const char8_t* str, std::size_t len);
+::toml::value operator""_toml(const char8_t* str, std::size_t len);
 #endif
 
 } // toml_literals
@@ -16805,7 +16805,7 @@ inline namespace toml_literals
 {
 
 TOML11_INLINE ::toml::value
-operator"" _toml(const char* str, std::size_t len)
+operator""_toml(const char* str, std::size_t len)
 {
     if(len == 0)
     {
@@ -16836,7 +16836,7 @@ operator"" _toml(const char* str, std::size_t len)
 // value of u8"" literal has been changed from char to char8_t and char8_t is
 // NOT compatible to char
 TOML11_INLINE ::toml::value
-operator"" _toml(const char8_t* str, std::size_t len)
+operator""_toml(const char8_t* str, std::size_t len)
 {
     if(len == 0)
     {
