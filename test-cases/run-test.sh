f6dc58b27063a83fff7fb23bd0557b81d86c8c07
Frances Wingerter <fw@immunant.com>
2026-06-17 15:46:40 -0400

    test-cases: save/compare golden output

--- test-cases/run-test.sh
+++ test-cases/run-test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 test_case=$1
+golden_file=${test_case}/main.expected.rs
+out_dir=/tmp/hayroll-output/${test_case}
 
 set -x
-exec ../build/hayroll transpile ${test_case}/compile_commands.json --output-dir /tmp/hayroll-output/${test_case}
+../build/hayroll transpile ${test_case}/compile_commands.json --output-dir ${out_dir} || exit 1
+
+[ ! -e ${golden_file} ] && cp ${out_dir}/src/main.rs ${golden_file}
+exec diff ${out_dir}/src/main.rs ${golden_file}
