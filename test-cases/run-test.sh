135070080ce34c6b1800300f53cd97953550838a
Frances Wingerter <fw@immunant.com>
2026-07-01 13:00:50 -0400

    run-test: pass -vv

--- test-cases/run-test.sh
+++ test-cases/run-test.sh
@@ -4,7 +4,7 @@ golden_file=${test_case}/main.expected.rs
 out_dir=/tmp/hayroll-output/${test_case}
 
 set -x
-../build/hayroll transpile ${test_case}/compile_commands.json --output-dir ${out_dir} || exit 1
+../build/hayroll -vv transpile ${test_case}/compile_commands.json --output-dir ${out_dir} || exit 1
 
 [ ! -e ${golden_file} ] && cp ${out_dir}/src/main.rs ${golden_file}
 exec diff ${out_dir}/src/main.rs ${golden_file}
