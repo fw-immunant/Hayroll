38714ec5bb797685a5c5695cded0d5202ad6feeb
Frances Wingerter <fw@immunant.com>
2026-07-01 12:59:45 -0400

    remove sphincs haraka defineset hack

--- src/PremiseTree.hpp
+++ src/PremiseTree.hpp
@@ -189,7 +189,6 @@ struct PremiseTree
     DefineSet getDefineSet() const
     {
         auto set = DefineSet(getModel());
-        set.defines["PARAMS=sphincs-haraka-128s"] = std::nullopt;
         return set;
     }
 
